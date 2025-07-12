1220d2c2017f58baaf049a648d2ca7ca20a90482
khen1 <abesamiskheni@gmail.com>
2025-07-13 02:55:25 +0800

    new features

---
+++ src/config/upstash.js
@@ -0,0 +1,14 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// create a ratelimiter for 50 requests per 60seconds
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(100,"60 s")
+})
+
+export default ratelimit;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,7 +12,12 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
+app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
