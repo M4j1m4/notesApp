fd5ce55d1f0153b13b9df2534c91188ecc703648
khen1 <abesamiskheni@gmail.com>
2025-07-17 02:47:21 +0800

    Update frontend components and pages

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -33,7 +33,9 @@ const NoteCard = ({note, setNotes}) => {
                     {formatDate(new Date(note.createdAt))}
                 </span>
                 <div className='flex items-center gap-1'>
-                    <PenSquareIcon className='size-4'/>
+                    <button className='btn btn-ghost btn-xs'>
+                      <PenSquareIcon className='size-4'/>
+                    </button>
                     <button className='btn btn-ghost btn-xs text-error'>
                         <Trash2Icon className='size-4' onClick={(e) => handleDelete(e,note._id)}/>
                     </button>
